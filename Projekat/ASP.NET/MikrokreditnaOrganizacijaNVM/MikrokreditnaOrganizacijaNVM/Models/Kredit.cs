dd92b733a2d3bdc48fc5b56fad46bbf9358d9b4f
mdelic3 <mdelic3@etf.unsa.ba>
2018-06-06 03:18:10 +0200

    ASP.NET pocetak

---
+++ Projekat/ASP.NET/MikrokreditnaOrganizacijaNVM/MikrokreditnaOrganizacijaNVM/Models/Kredit.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MikrokreditnaOrganizacijaNVM.Models
+{
+    public class Kredit
+    {
+    }
+}
